5a8077029ad8965957b2047b2a998e29d95a1024
Joseph Willrich Lutalo <joewillrich@gmail.com>
2022-09-19 10:56:26 +0300

    add meta data and the e-candle service

---
+++ ecandle/script.js
@@ -0,0 +1,8 @@
+// the nav
+$(".toggleMenu").on('click', function(){
+  $("#mainMenu").toggleClass('open');
+});
+
+// the e-candle
+
+particlesJS("particles-js", {"particles":{"number":{"value":300,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffa500"},"shape":{"type":"circle","stroke":{"width":0,"color":"##FF7034"},"polygon":{"nb_sides":5},"image":{"src":"","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#414141","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":500,"rotateY":600}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":100,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":60,"duration":1},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":false});
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// the e-candles...
 tl = gsap.timeline();
 tl.set(".fire, .smoke", {visibility:"hidden"});
 tl.to(".candle", {y:160, duration: 20, stagger:3, delay:1, ease: "sine"},"<");
@@ -7,7 +8,7 @@ tl.set(".fire", {visibility:"hidden", stagger:3 }, "-=9");
 tl.set(".smoke", {visibility:"visible", stagger:3}, "-=9");
 tl.set(".smoke", {visibility:"hidden", delay:.5, stagger:3}, "-=7.5");
 
-
+// the nav
 $(".toggleMenu").on('click', function(){
   $("#mainMenu").toggleClass('open');
 });
